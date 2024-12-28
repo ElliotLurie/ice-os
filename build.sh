db57d10c5e18e9467bd8448d7fd0075747c0f5e4
Elliot <ElliotLurie@mailo.com>
2024-12-28 03:28:37 -0500

    I shoudln't be awake right now

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Remove unnecessary base packages
-rpm-ostree override uninstall atril firefox mousepad pavucontrol ristretto
+rpm-ostree override remove atril firefox mousepad pavucontrol ristretto
 rpm-ostree install gnome-software zsh{-,autosuggestions,syntax-highlighting}
