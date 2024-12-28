ad3783c426eba1cf31f830488c93bb0ec4d0e3a2
Elliot <ElliotLurie@mailo.com>
2024-12-28 03:07:02 -0500

    Create images!

--- build.sh
+++ build.sh
@@ -4,17 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
+# Remove unnecessary base packages
+rpm-ostree remove atril firefox mousepad pavucontrol ristretto
+rpm-ostree install gnome-software zsh{-,autosuggestions,syntax-highlighting}
